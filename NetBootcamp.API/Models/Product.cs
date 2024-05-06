ba3d579ced65756c5e2ddfa5764d4690a822c9f7
aydindemir1 <aynndemr@gmail.com>
2024-05-06 23:39:34 +0300

    Add project files.

---
+++ NetBootcamp.API/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace NetBootcamp.API.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default;
+        public decimal Price { get; set; }
+        public DateTime Created { get; set; } = new();
+
+        public string Barcode { get; init; } = default;
+    }
+}
